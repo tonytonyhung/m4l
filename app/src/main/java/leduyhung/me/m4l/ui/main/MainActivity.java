c6b35227ace57cab164404cf24030cb9bd1b747f
tonytonyhung <tony.leduyhung@gmail.com>
2018-06-18 13:42:53 +0700

    del some file

--- app/src/main/java/leduyhung/me/m4l/ui/main/MainActivity.java
+++ app/src/main/java/leduyhung/me/m4l/ui/main/MainActivity.java
@@ -1,15 +1,13 @@
-package leduyhung.me.m4l.ui.login;
+package leduyhung.me.m4l.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import leduyhung.me.m4l.R;
-
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
