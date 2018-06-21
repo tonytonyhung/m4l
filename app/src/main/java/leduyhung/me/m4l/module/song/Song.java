9465a5bd5caddff49161c04cc69752cfd20af6f4
tonytonyhung <tony.leduyhung@gmail.com>
2018-06-21 22:51:13 +0700

    song module

--- app/src/main/java/leduyhung/me/m4l/module/song/Song.java
+++ app/src/main/java/leduyhung/me/m4l/module/song/Song.java
@@ -1,8 +1,21 @@
 package leduyhung.me.m4l.module.song;
 
+import android.app.Activity;
+import android.content.Context;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 import leduyhung.me.m4l.module.song.data.SongInfo;
+import leduyhung.me.m4l.rest.BaseRestApi;
+import leduyhung.me.m4l.util.ClientUtil;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Song {
 
@@ -10,6 +23,7 @@ public class Song {
     private int total_page;
     private int current_page;
     private List<SongInfo> data;
+    private transient Call<Song> call;
 
     public int getTotal_item() {
         return total_item;
@@ -25,5 +39,48 @@ public class Song {
 
     public List<SongInfo> getData() {
         return data;
+    }
+
+    public void getListSong(final Context ctx, String name, int page) {
+
+        if (call == null && !((Activity) ctx).isFinishing()) {
+            call = BaseRestApi.request().getListSong(ClientUtil.getVersionCode(ctx), ClientUtil.getPakageName(ctx), page);
+            call.enqueue(new Callback<Song>() {
+                @Override
+                public void onResponse(Call<Song> call, Response<Song> response) {
+                    if (response.isSuccessful()) {
+
+                    } else {
+
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Song> call, Throwable t) {
+                    if (!ClientUtil.isConnectInternet(ctx)) {
+
+                    } else {
+
+                        if (t instanceof TimeoutException || t instanceof SocketTimeoutException || t instanceof UnknownHostException) {
+
+                        } else {
+
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public void loveSong(final Context ctx) {
+        // TODO: love song
+    }
+
+    public void downloadSong(final Context ctx) {
+        // TODO: download song
+    }
+
+    public void getSongLocal(final Context ctx) {
+        // TODO: get song local
     }
 }
--- app/src/main/java/leduyhung/me/m4l/rest/TaskApi.java
+++ app/src/main/java/leduyhung/me/m4l/rest/TaskApi.java
@@ -1,14 +1,22 @@
 package leduyhung.me.m4l.rest;
 
+import java.util.List;
+
+import leduyhung.me.m4l.module.song.Song;
 import leduyhung.me.m4l.module.user.User;
 import leduyhung.me.m4l.module.user.data.UserInfo;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface TaskApi {
 
     @POST("/v1/auth")
     Call<UserInfo> login(@Header("version") int version, @Header("pakage") String pakage, @Body User user);
+
+    @GET("/v1/songs")
+    Call<Song> getListSong(@Header("version") int version, @Header("pakage") String pakage, @Query("page") int page);
 }
